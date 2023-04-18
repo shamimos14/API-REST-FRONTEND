12c3d52b83864ae57a12fdc1c1884da481b5dc80
shamimos14 <125527188+shamimos14@users.noreply.github.com>
2023-04-17 23:02:00 -0500

    fix(axios-config): adjustments base url

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://api-rest-nodejs-inventario.onrender.com'
 });
 
 export {
